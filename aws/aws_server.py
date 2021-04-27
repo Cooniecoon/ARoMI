6486079b44cec27093ec97acf5aad6f426cda716
cooniecoon <hoony3355@gmail.com>
2021-04-27 14:29:36 +0900

    fps testing

--- aws/aws_server.py
+++ aws/aws_server.py
@@ -15,16 +15,16 @@ s.listen(True)
 cam_client, addr = s.accept()
 print("connected")
 
-# 수신에 사용될 내 ip와 내 port번호
-TCP_IP = "ec2-13-209-8-64.ap-northeast-2.compute.amazonaws.com"
-TCP_PORT = 1997
+# # 수신에 사용될 내 ip와 내 port번호
+# TCP_IP = "ec2-13-209-8-64.ap-northeast-2.compute.amazonaws.com"
+# TCP_PORT = 1997
 
-# TCP소켓 열고 수신 대기
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(True)
-fps_plotter, addr = s.accept()
-print("connected")
+# # TCP소켓 열고 수신 대기
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((TCP_IP, TCP_PORT))
+# s.listen(True)
+# fps_plotter, addr = s.accept()
+# print("connected")
 
 
 
@@ -34,14 +34,14 @@ while True:
     
     image = recv_img_from(cam_client)
     
-    send_image_to(image,cam_client,dsize=(640, 480))
+    send_image_to(image,cam_client,dsize=(300, 300))
     dt = time.time() - start
     print("fps : {:.2f}".format(1 / dt))
 
 
-    msg="{:.2f}".format(1 / dt)
-    fps_plotter.send(msg.encode())
-    recv_check(fps_plotter)
+    # msg="{:.2f}".format(1 / dt)
+    # fps_plotter.send(msg.encode())
+    # recv_check(fps_plotter)
     
     if cv2.waitKey(10) == 27:
         break
--- aws/fps_plot.py
+++ aws/fps_plot.py
@@ -3,12 +3,22 @@ import cv2
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from collections import deque
+
+
 from socket_funcs import *
 
 with open('message_code.json', 'r') as f:
     messages = json.load(f)
 
+a1 = deque([0]*200)
+ax = plt.axes(xlim=(0, 30), ylim=(0, 50))
 
+
+line, = plt.plot(a1)
+plt.ion()
+plt.ylim([0,50])
+plt.show()
 
 # 연결할 서버(수신단)의 ip주소와 port번호
 TCP_IP = "ec2-13-209-8-64.ap-northeast-2.compute.amazonaws.com"
@@ -22,8 +32,12 @@ while True:
     fps=aws_server.recv(4)
     msg=messages['roger']
     aws_server.send(msg.encode())
-    fps=fps.decode()
-    
+    fps=float(fps.decode())
 
+    a1.appendleft(fps)
+    datatoplot = a1.pop()
+    line.set_ydata(a1)
+    plt.draw()
+    plt.pause(0.0001) 
 
 aws_server.close()
