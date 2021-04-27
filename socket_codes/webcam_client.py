4eb2e2a1ff8d89187f096d8541a793dea3cf9d7d
cooniecoon <hoony3355@gmail.com>
2021-04-27 12:50:51 +0900

    Socket image transfer edited

--- socket_codes/webcam_client.py
+++ socket_codes/webcam_client.py
@@ -2,41 +2,30 @@ import socket
 import cv2
 import numpy as np
 import time
+from socket_funcs import *
 
-# socket 수신 버퍼를 읽어서 반환하는 함수
-def recvall(sock, count):
-    buf = b""
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf:
-            return None
-        buf += newbuf
-        count -= len(newbuf)
 
-    return buf
 
 
 # 연결할 서버(수신단)의 ip주소와 port번호
-TCP_IP = "192.168.35.139"
-TCP_PORT = 6666
+TCP_IP = "127.0.0.1"
+TCP_PORT = 1997
 
 # 송신을 위한 socket 준비
-sock = socket.socket()
-sock.connect((TCP_IP, TCP_PORT))
+cam_server = socket.socket()
+cam_server.connect((TCP_IP, TCP_PORT))
 
 while True:
     start = time.time()
-    newbuf = sock.recv(16)
-    length = newbuf.decode()
-    img_data = recvall(sock, int(length))
-    data = np.frombuffer(img_data, dtype="uint8")
-    image = cv2.imdecode(data, 1)
+    
+    image = recv_img(cam_server)
+    
     cv2.imshow("CLIENT", image)
-
+    send_image(image,cam_server,dsize=(640, 480))
     dt = time.time() - start
     print("fps : {:.2f}".format(1 / dt))
     if cv2.waitKey(10) == 27:
         break
-sock.close()
-capture.release()
+cam_server.close()
+
 cv2.destroyAllWindows()
--- socket_codes/webcam_server.py
+++ socket_codes/webcam_server.py
@@ -2,25 +2,10 @@ import socket
 import cv2
 import numpy as np
 import time
+from socket_funcs import *
 
-
-def recvall(sock, count):
-    buf = b""
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf:
-            return None
-        buf += newbuf
-        count -= len(newbuf)
-
-    return buf
-
-def recv_check():
-    while True:
-        msg=conn.recv(5).decode()
-        print(msg)
-        if msg=='roger':
-            break
+with open('message_code.json', 'r') as f:
+    messages = json.load(f)
 
 cam=cv2.VideoCapture(0)
 cam.set(3,640)
@@ -29,35 +14,29 @@ _,img=cam.read()
 
 # 수신에 사용될 내 ip와 내 port번호
 TCP_IP = "127.0.0.1"
-TCP_PORT = 4242
+TCP_PORT = 1997
 
 # TCP소켓 열고 수신 대기
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(True)
-conn, addr = s.accept()
+aws_client, addr = s.accept()
 print("connected")
 
 while True:
-
+    start = time.time()
     _,img=cam.read()
-    img=cv2.resize(img, dsize=(432, 368), interpolation=cv2.INTER_AREA)
-    # send image to client
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-    result, imgencode = cv2.imencode(".jpg", img, encode_param)
-    data = np.array(imgencode)
-    stringData = data.tobytes()
 
-    # String 형태로 변환한 이미지를 socket을 통해서 전송
-    data_len = len(stringData)
+    send_image(img,aws_client,dsize=(432, 368))
 
-    str_data_len = str(data_len).encode().ljust(16)
 
-    conn.send(str_data_len)
-    conn.send(stringData)
-    recv_check()
-    
-    # cv2.imshow("SERVER", im0)
+    img_recv=recv_img(aws_client)
+    dt = time.time() - start
+    cv2.putText(img_recv, text="fps : {:.2f}".format(1 / dt), org=(30, 30), 
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, 
+                        color=(255, 255, 0), thickness=2)
+
+    cv2.imshow("SERVER", img_recv)
     if cv2.waitKey(1) == 27:
         break
 cv2.destroyAllWindows()
